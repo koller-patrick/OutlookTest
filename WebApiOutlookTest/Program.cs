c3bdd28465ed9b22afa5dea98b16a32d0af08dd3
Patrick Koller <patrick.koller@primesoft-group.com>
2024-01-31 11:21:32 +0100

    init repo

---
+++ WebApiOutlookTest/Program.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+
+app.Map("/api/signature", () => Results.Ok("<p>Signature <b>Test</b></p>"));
+
+
+app.Run();
+
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
